8f601fa4f41ed3d0fa791da7f82197e31d43ad19
nguyendangvu221 <nguyendangvu221@gmail.com>
2023-02-03 15:39:50 +0700

    update final

--- lib/src/views/match_view/contries_match.dart
+++ lib/src/views/match_view/contries_match.dart
@@ -3,8 +3,10 @@ import 'package:intl/intl.dart';
 import 'package:world_cup/src/views/worldcup_view/flag.dart';
 
 class CountriesMatch extends StatelessWidget {
-  final int homeScorers;
-  final int awayScorers;
+  final String homeScorers;
+  final String awayScorers;
+  final int homeScore;
+  final int awayScore;
   final String homeFlag;
   final String awayFlag;
   final String homeTeamEn;
@@ -15,6 +17,8 @@ class CountriesMatch extends StatelessWidget {
   const CountriesMatch(
       {required this.homeFlag,
       required this.homeScorers,
+      required this.homeScore,
+      required this.awayScore,
       required this.awayScorers,
       required this.homeTeamEn,
       required this.awayFlag,
@@ -27,31 +31,22 @@ class CountriesMatch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   leading: IconButton(
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //     },
-      //     icon: const Icon(Icons.arrow_back),
-      //   ),
-      //   toolbarHeight: 0,
-      // ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              margin: const EdgeInsets.only(top: 20),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              "asset/hinh-nen-bong-da.jpg",
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
               child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      "asset/hinh-nen-bong-da.jpg",
-                    ),
-                    fit: BoxFit.fill,
-                  ),
-                ),
+                margin: const EdgeInsets.only(top: 20),
                 child: Column(
                   children: [
                     Container(
@@ -65,6 +60,9 @@ class CountriesMatch extends StatelessWidget {
                               color: Colors.white,
                               size: 30,
                             ))),
+                    const SizedBox(
+                      height: 40,
+                    ),
                     Text(
                       "$group, " "${dateFormat.format(dateTime)}",
                       style: const TextStyle(fontSize: 17, color: Colors.white),
@@ -96,7 +94,7 @@ class CountriesMatch extends StatelessWidget {
                             flex: 2,
                             child: Center(
                               child: Text(
-                                "$homeScorers - $awayScorers",
+                                "$homeScore - $awayScore",
                                 style: const TextStyle(
                                     fontSize: 35,
                                     fontWeight: FontWeight.w700,
@@ -126,38 +124,53 @@ class CountriesMatch extends StatelessWidget {
                         ],
                       ),
                     ),
-                    // Container(
-                    //   margin: const EdgeInsets.only(top: 20, left: 50),
-                    //   child: Row(
-                    //     children: [
-                    //       Expanded(
-                    //           flex: 4,
-                    //           child: Text(
-                    //             homeTeamEn,
-                    //             style: const TextStyle(
-                    //                 fontSize: 20, color: Colors.white),
-                    //           )),
-                    //       Expanded(
-                    //           flex: 2,
-                    //           child: Text(
-                    //             awayTeamEn,
-                    //             style: const TextStyle(
-                    //                 fontSize: 20, color: Colors.white),
-                    //           ))
-                    //   ],
-                    // ),
-                    // ),
                   ],
                 ),
               ),
             ),
-          ),
-          Expanded(
-              flex: 2,
-              child: Column(
-                children: [Text("expanded 2")]
-              ))
-        ],
+            Expanded(
+                child: Card(
+              color: Colors.white.withOpacity(0.6),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: Column(children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      homeScorers != 'null'
+                          ? homeScorers.toString().replaceAll(',', '\n')
+                          : '',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      textAlign: TextAlign.end,
+                    )),
+                    Expanded(
+                        child: Image.asset(
+                      "asset/football.png",
+                      scale: 5.5,
+                    )),
+                    Expanded(
+                        child: Text(
+                      awayScorers != 'null'
+                          ? awayScorers.toString().replaceAll(',', '\n')
+                          : '',
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    )),
+                  ],
+                )
+              ]),
+            ))
+          ],
+        ),
       ),
     );
   }
